6f0332a4279ff4056b4605a7eebbbc1ccf19ee90
Abin Joseph <itsmeabjs@gmail.com>
2021-10-26 21:33:16 +0530

    update

--- dfa.c
+++ dfa.c
@@ -1,90 +1,102 @@
 #include <stdio.h>
 #include <string.h>
 int dfa = 0;
-void start(char c)
+void q0(char c)
 {
-	if (c == 'a') {
-		dfa = 1;
-	}
-	else if (c == 'b') {
-		dfa = 3;
-	}
-	else {
-		dfa = -1;
-	}
+    if (c == 'a')
+    {
+        dfa = 1;
+    }
+    else if (c == 'b')
+    {
+        dfa = 3;
+    }
+    else
+    {
+        dfa = -1;
+    }
 }
-void state1(char c)
+void q1(char c)
 {
-	if (c == 'a') {
-		dfa = 2;
-	}
-	else if (c == 'b') {
-		dfa = 4;
-	}
-	else {
-		dfa = -1;
-	}
+    if (c == 'a')
+    {
+        dfa = 2;
+    }
+    else if (c == 'b')
+    {
+        dfa = 4;
+    }
+    else
+    {
+        dfa = -1;
+    }
 }
-void state2(char c)
+void q2(char c)
 {
-	if (c == 'b') {
-		dfa = 3;
-	}
-	else if (c == 'a') {
-		dfa = 1;
-	}
-	else {
-		dfa = -1;
-	}
+    if (c == 'b')
+    {
+        dfa = 3;
+    }
+    else if (c == 'a')
+    {
+        dfa = 1;
+    }
+    else
+    {
+        dfa = -1;
+    }
 }
-void state3(char c)
+void q3(char c)
 {
-	if (c == 'b') {
-		dfa = 3;
-	}
-	else if (c == 'a') {
-		dfa = 4;
-	}
-	else {
-		dfa = -1;
-	}
+    if (c == 'b')
+    {
+        dfa = 3;
+    }
+    else if (c == 'a')
+    {
+        dfa = 4;
+    }
+    else
+    {
+        dfa = -1;
+    }
 }
-void state4(char c)
+void q4(char c)
 {
-	dfa = -1;
+    dfa = -1;
 }
 
 int isAccepted(char str[])
 {
-	int i, len = strlen(str);
-	for (i = 0; i < len; i++) {
-		if (dfa == 0)
-			start(str[i]);
-		else if (dfa == 1)
-			state1(str[i]);
-		else if (dfa == 2)
-			state2(str[i]);
-		else if (dfa == 3)
-			state3(str[i]);
-		else if (dfa == 4)
-			state4(str[i]);
-		else
-			return 0;
-	}
-	if (dfa == 3)
-		return 1;
-	else
-		return 0;
+    int i, len = strlen(str);
+    for (i = 0; i < len; i++)
+    {
+        if (dfa == 0)
+            q0(str[i]);
+        else if (dfa == 1)
+            q1(str[i]);
+        else if (dfa == 2)
+            q2(str[i]);
+        else if (dfa == 3)
+            q3(str[i]);
+        else if (dfa == 4)
+            q4(str[i]);
+        else
+            return 0;
+    }
+    if (dfa == 3)
+        return 1;
+    else
+        return 0;
 }
-
-// driver code
 int main()
 {
-	char str[] = "aaaaaabbbb";
-	if (isAccepted(str))
-		printf("ACCEPTED");
-	else
-		printf("NOT ACCEPTED");
-	return 0;
+    char str[100];
+    printf("Enter The Input String:");
+    gets(str);
+    if (isAccepted(str))
+        printf("String ACCEPTED");
+    else
+        printf("String NOT ACCEPTED");
+    return 0;
 }
-
