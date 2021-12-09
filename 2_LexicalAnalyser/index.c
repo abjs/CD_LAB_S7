44293d9fbeb85901bb100829c194fccf7990304a
Abin Joseph <itsmeabjs@gmail.com>
2021-12-09 22:31:36 +0530

    LexicalAnalyser done

--- 2_LexicalAnalyser/index.c
+++ 2_LexicalAnalyser/index.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <string.h>
+int isAlphabet(char x);
+int isNumber(int x);
+int isAnIdentifier(char str[]);
+int isAnKeyWords(char str[]);
+int haveKeyWord(char str[]);
+int isSymbol(char str[]);
+int isOperator(char str[]);
+void main()
+{
+
+    FILE *InputProgramFile = fopen("Input.txt", "r");
+    char programTemp[100];
+    while (fscanf(InputProgramFile, "%s", programTemp) != EOF)
+    {
+        if (isAnKeyWords(programTemp) == 1)
+        {
+            printf("%s is a keyword\n", programTemp);
+        }
+        else
+        {
+            if (!haveKeyWord(programTemp))
+            {
+                if (isAnIdentifier(programTemp))
+                {
+                    printf("%s is an identifier\n", programTemp);
+                }
+                else if (isOperator(programTemp))
+                {
+                    printf("%s is a Operator\n", programTemp);
+                }
+                else if (isSymbol(programTemp))
+                {
+                    printf("%s is a symbol\n", programTemp);
+                }
+            }
+        }
+    }
+}
+
 int isAlphabet(char x)
 {
     int i = 0;
     char c_low = 'a', c_up = 'A';
     int isLetter = 0;
-    for (i = 0; i <= 26; i++)
+    for (i = 0; i < 26; i++)
     {
         if (x == c_low + i)
         {
             isLetter = 1;
         }
     }
-    for (i = 0; i <= 26; i++)
+    for (i = 0; i < 26; i++)
     {
         if (x == c_up + i)
         {
@@ -28,7 +67,6 @@ int isAlphabet(char x)
         return 1;
     }
 }
-
 int isNumber(int x)
 {
     if (x >= '0' && x <= '9')
@@ -40,18 +78,67 @@ int isNumber(int x)
         return 0;
     }
 }
-
 int isAnIdentifier(char str[])
 {
-    int i = 0, flag = 0;
-    if (isAlphabet(str[0]))
+    int flag = 0;
+    if (isAlphabet(str[0]) == 1)
     {
-        for (i = 1; i <= strlen(str); i++)
+        flag = 1;
+    }
+    for (int i = 0; i < strlen(str); i++)
+    {
+
+        if (isAlphabet(str[i]) || isNumber(str[i]) || str[i] == '_')
         {
-            if (isAlphabet(str[i]) || isNumber(str[i] || str[i] == '_'))
-            {
-                return 1;
-            }
+            flag = 1;
+        }
+        else
+        {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+int isAnKeyWords(char str[])
+{
+    FILE *KeywordsFile = fopen("Keywords.txt", "r");
+    char keywordsTemp[100];
+    int flag = 0;
+    while (fscanf(KeywordsFile, "%s", keywordsTemp) != EOF)
+    {
+        if (strcmp(str, keywordsTemp) == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    return flag;
+}
+int haveKeyWord(char str[])
+{
+    FILE *O_Keyword = fopen("O_Keyword.txt", "w");
+    int haveOpeningBracket = 0, openingBracketIndex;
+    char temp[100];
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '(')
+        {
+            haveOpeningBracket = 1;
+            openingBracketIndex = i;
+        }
+    }
+    if (haveOpeningBracket == 1)
+    {
+        for (int i = 0; i < openingBracketIndex; i++)
+        {
+            temp[i] = str[i];
+        }
+        temp[openingBracketIndex] = '\0';
+        if (isAnKeyWords(temp))
+        {
+            printf("%s is a keyword\n", temp);
+            return 1;
         }
     }
     else
@@ -59,66 +146,34 @@ int isAnIdentifier(char str[])
         return 0;
     }
 }
-int main()
+int isSymbol(char str[])
 {
-    char prog[100], key[100], opt[100], sym[100], temp[100];
-    FILE *keyWords = fopen("Keywords.txt", "r");
-    FILE *Operators = fopen("Operators.txt", "r");
     FILE *Symbols = fopen("Symbols.txt", "r");
-    FILE *Input = fopen("Input.txt", "r");
-    int flag, i;
-    while (fscanf(Input, "%s", prog) != EOF)
+    char symbol[100];
+    int flag = 0;
+    while (fscanf(Symbols, "%s", symbol) != EOF)
     {
-        rewind(keyWords);
-        flag = 0;
-        while (fscanf(keyWords, "%s", key) != EOF)
+        if (strcmp(str, symbol) == 0)
         {
-
-            if (strcmp(key, prog) == 0)
-            {
-                printf("%s\n", prog);
-                printf("\n Keyword - %s\n", key);
-                flag = 1;
-                break;
-            }
-        }
-        rewind(Operators);
-        while (fscanf(Operators, "%s", opt) != EOF)
-        {
-            if (strcmp(opt, prog) == 0)
-            {
-                printf("%s\n", opt);
-                printf("\n Operator - %s\n", opt);
-                break;
-                flag = 1;
-            }
-        }
-        if (flag == 0)
-        {
-            i = 0;
-
-            while (strlen(prog) != i)
-            {
-                temp[i] = prog[i];
-                rewind(keyWords);
-                while (fscanf(keyWords, "%s", key) != EOF)
-                {
-                    if (strcmp(temp, key) == 0)
-                    {
-                        printf("\n Keyword - %s\n", temp);
-                        flag = 1;
-                        break;
-                    }
-                }
-                i++;
-            }
-        }
-        if (flag != 1)
-        {
-            if (isAnIdentifier(prog))
-            {
-                printf("Identifier : %s\n", prog);
-            }
+            flag = 1;
+            break;
         }
     }
+    return flag;
+}
+int isOperator(char str[])
+{
+    int flag = 1;
+    char Operator[100];
+    FILE *Operators = fopen("Operators.txt", "r");
+    while (fscanf(Operators, "%s", Operator) != EOF)
+    {
+
+        if (strcmp(str, Operator) == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    return flag;
 }
